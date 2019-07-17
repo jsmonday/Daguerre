3a58020b0d7aebb17f82f250f6c6b5d0fe159d32
Michele Riva <master@micheleriva.it>
2019-07-17 12:23:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<img src="./docs/daguerre.png" alt="Laika Daguerre" />
+<br />
+
+**Daguerre** is a **serverless** image manipulation function that lives on **Google Cloud Functions**.
+
+# License
+[GNU GPLv3](/LICENSE.md)
---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions        = require("firebase-functions");
+const Storage          = require("@google-cloud/storage");
+const { tmpDir, join } = require("os");
+const { dirname }      = require("path");
+const sharp            = require("sharp");
+const fs               = require("fs-extra");
+const gcs              = Storage();
+
+const resizeImg = functions.storage.object().onFinalize(async (object) => {
+  const bucket    = gcs.bucket(object.bucket);
+  const filePath  = object.name;
+  const fileName  = filePath.split("/").pop();
+  const bucketDir = dirname(filePath);
+
+  const workingDir  = join(tmpDir(), "resize");
+  const tmpFilePath = join(workingDir, "source.png");
+
+  if (fileName.includes("r@") || !object.contentType.includes("image")) {
+    console.log(`Exiting function`);
+    return false;
+  }
+
+  await fs.ensureDir(workingDir);
+  await bucket.file(filePath).download({ destination: tmpFilePath });
+
+  const sizes = [1920, 100];
+
+  const uploadPromises = sizes.map(async (size) => {
+    const imgName = `${fileName}@${size}`;
+    const imgPath = join(workingDir, imgName);
+    await sharp(tmpFilePath).resize({ width: size }).toFile(imgPath);
+
+    return bucket.upload(imgPath, {
+      destination: join(bucketDir, imgName)
+    });
+  });
+
+  await Promise.all(uploadPromises);
+
+  return fs.remove(workingDir);
+
+});
